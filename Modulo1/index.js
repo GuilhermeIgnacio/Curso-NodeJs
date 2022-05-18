c1a76b1f94382a0b1d87eb421443575e03d11211
Guilherme Ignácio <stormierearth9@gmail.com>
2022-05-18 14:55:45 -0300

    Criando primeiro projeto

---
+++ Modulo1/index.js
@@ -0,0 +1,9 @@
+// let nome = "Guilherme";
+// let idade = "19";
+
+let pessoa = {
+  nome: "Guilherme",
+  idade: 19,
+};
+
+console.log(`Meu nome é ${pessoa.nome} e eu tenho ${pessoa.idade} anos`);
