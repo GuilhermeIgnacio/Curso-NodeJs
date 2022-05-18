bd9593fe8ef993bc0b70837449c186045f341e7d
Guilherme Ignácio <stormierearth9@gmail.com>
2022-05-18 15:13:29 -0300

    Instalando o Typescript no projeto

--- Modulo1/index.js
+++
@@ -1,9 +0,0 @@
-// let nome = "Guilherme";
-// let idade = "19";
-
-let pessoa = {
-  nome: "Guilherme",
-  idade: 19,
-};
-
-console.log(`Meu nome é ${pessoa.nome} e eu tenho ${pessoa.idade} anos`);
---
+++ Modulo1/src/index.ts
@@ -0,0 +1,4 @@
+let nome: string = "Guilherme";
+let idade: number = 19;
+
+console.log(`Meu nome é ${nome} e eu tenho ${idade}`);
