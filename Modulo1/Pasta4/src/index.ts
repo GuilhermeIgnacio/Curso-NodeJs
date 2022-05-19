b163dc832502d54d8ec2a42e2aaa99177ffb6baf
Guilherme Ignácio <stormierearth9@gmail.com>
2022-05-19 19:20:39 -0300

    Instalando e configurando o Mustache

---
+++ Modulo1/Pasta4/src/index.ts
@@ -0,0 +1,26 @@
+import express, { Request, Response } from "express";
+import path from "path";
+import mustache from "mustache-express";
+
+import mainRoutes from "./routes/router";
+import painelRoutes from "./routes/painel";
+
+const app = express();
+const port = 3000;
+
+app.use(mainRoutes);
+app.use("/painel", painelRoutes);
+
+app.set("view engine", "mustache");
+app.set("views", path.join(__dirname, "views"));
+app.engine("mustache", mustache());
+
+app.use(express.static(path.join(__dirname, "../public")));
+
+app.use((req: Request, res: Response) => {
+  res.status(404).send("Página Não Encontrada");
+});
+
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta: ${port}`);
+});
