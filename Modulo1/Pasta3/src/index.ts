ae14c33726a2c0b4255478c51e76ab78e70fa582
Guilherme Ignácio <stormierearth9@gmail.com>
2022-05-19 14:21:00 -0300

    Pasta pública e arquivos estáticos

--- Modulo1/Pasta3/src/index.ts
+++ Modulo1/Pasta3/src/index.ts
@@ -1,12 +1,15 @@
 import express, { Request, Response } from "express";
 import mainRoutes from "./routes/router";
 import painelRoutes from "./routes/painel";
+import path from "path";
 
 const app = express();
 const port = 3000;
 
 app.use(mainRoutes);
 app.use("/painel", painelRoutes);
+
+app.use(express.static(path.join(__dirname, "../public")));
 
 app.use((req: Request, res: Response) => {
   res.status(404).send("Página Não Encontrada");
