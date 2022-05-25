2c35ddfb3c77c1e75716d3673eb34ba0474af18b
Guilherme Ignácio <stormierearth9@gmail.com>
2022-05-25 16:58:24 -0300

    Trabalhando com Models

--- Modulo1/Pasta4/src/controllers/homeController.ts
+++ Modulo1/Pasta4/src/controllers/homeController.ts
@@ -1,16 +1,15 @@
 import { Request, Response } from "express";
+import { Product } from "../models/Product";
 
 export const home = (req: Request, res: Response) => {
+  let list = Product.getAll();
+
   let user = {
     nome: "Guilherme",
     sobrenome: "Ignácio",
     idade: 19,
     showWelcome: false,
-    products: [
-      { title: "Produto X", price: 10 },
-      { title: "Produto Y", price: 15 },
-      { title: "Produto Z", price: 20 },
-    ],
+    products: list,
     // frases: ["Frase 1", "Frase 2", "Frase 3"],
   };
 
